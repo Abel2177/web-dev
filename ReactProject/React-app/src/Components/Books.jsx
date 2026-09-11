67104f59585f456554647105450d2477d48c7e12
Abel2177 <abelaalemayehu10@gmail.com>
2026-09-11 12:36:31 +0300

    react project fixing

--- ReactProject/React-app/src/Components/Books.jsx
+++ ReactProject/React-app/src/Components/Books.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function Books({ id, title, author, cover_id }) {
-    return (<div>
-        <img src={`https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`} alt={title} />
-        <h2>{title}</h2>
-        <p>{author}</p>
-    </div>
-    )
+function Books({ id, title, author, cover_id, favorite, onFavorite }) {
+    return (
+        <div>
+            <img
+                src={`https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`}
+                alt={title}
+            />
+
+            <h2>{title}</h2>
+
+            <p>{author}</p>
+
+            <button onClick={onFavorite}>
+                {favorite ? "❤️ Remove Favorite" : "♡ Add Favorite"}
+            </button>
+        </div>
+    );
 }
 
-export default Books
+export default Books;
